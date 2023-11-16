7f7e06a23f9eae4ee4e3146ee76f2aaa6329f710
NelsonGiraldo <n.giraldo@gbh.com.do>
2023-11-16 14:42:22 -0500

    Add project files.

---
+++ WebApiInventario/Data/WebApiInventarioContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiInventario.Models;
+
+namespace WebApiInventario.Data
+{
+    public class WebApiInventarioContext : DbContext
+    {
+        public WebApiInventarioContext (DbContextOptions<WebApiInventarioContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiInventario.Models.Inventario> Inventario { get; set; } = default!;
+    }
+}
---
+++ WebApiInventario/Models/Inventario.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiInventario.Models
+{
+    public class Inventario
+    {
+        public int Id { get; set; }
+        public int IdFactura { get; set; }
+        public int IdProducto { get; set;}
+        public string? EntradaProductos { get; set; }
+       public int Stock {  get; set; }
+    }
+}
